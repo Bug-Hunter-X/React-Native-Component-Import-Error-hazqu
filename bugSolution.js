457af881f9748e6a1a05db0f3ecca0eea8f0815e
BugHunter-X <askzenai@gmail.com>
2025-01-02 01:54:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,14 @@
+// Correct import
+import MyComponent from './MyComponent';
+
+// ... rest of your component code
+
+const MyScreen = () => {
+  return (
+    <View>
+      <MyComponent />
+    </View>
+  );
+};
+
+export default MyScreen;
